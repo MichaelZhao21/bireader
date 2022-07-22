ca81e44a840b26be5acd2ee356a8d1caa5e6577e
Michael Zhao <michaelzhao314@gmail.com>
2022-07-22 11:28:59 -0500

    Reformat files and remove useless utils file

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,27 +1,24 @@
 let cachedStorage = undefined;
 
-// https://stackoverflow.com/a/59787784
-const isEmpty = obj => {
-    for(const i in obj) {
-        return false;
-    }
-    return true;
+export const setStorage = async (val) => (
+    await chrome.storage.sync.set(val), (cachedStorage = val)
+);
+
+export const patchStorage = async (patch) => {
+    const prevStorage = await getStorage();
+    await setStorage({ ...prevStorage, ...patch });
 };
 
-export const setStorage = async val => (await chrome.storage.sync.set(val), cachedStorage = val);
-
-export const patchStorage = async patch => await setStorage({ ...(await getStorage()), ...patch });
-
 export const getStorage = async () => {
-    if(cachedStorage) return cachedStorage;
+    if (cachedStorage) return cachedStorage;
     const storage = chrome.storage.sync.get();
-    if(!storage || isEmpty(storage)) {
+    if (!storage || Object.keys(storage).length === 0) {
         return await setStorage({
             fixation: 0.5,
             saccade: 0,
             opacity: 1,
-            fontWeight: 700
+            fontWeight: 700,
         });
     }
-    return cachedStorage = storage;
+    return (cachedStorage = storage);
 };
